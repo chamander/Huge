cf87859f7fa337b27a4f387d5138b87678eceda7
Gavan Chan <chan.gavan+github@gmail.com>
2017-05-31 18:51:31 +1000

    Create Xcode project

---
+++ Source/Huge.h
@@ -0,0 +1,5 @@
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double HugeVersionNumber;
+FOUNDATION_EXPORT const unsigned char HugeVersionString[];
---
+++ Specs/HugeSpecs.swift
@@ -0,0 +1,15 @@
+
+final class HugeTests: XCTestCase {
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
+
+import XCTest
+
+@testable import Huge
